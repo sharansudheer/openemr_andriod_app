23317e11cd9302baa2193fe99fa38b2c3a09d5cd
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-02-15 23:54:11 +0530

    Update test.java

--- test.java
+++ test.java
@@ -168,3 +168,23 @@ public class RestController {
 	
 	}
 }
+package com.example.menu.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+      .mvcMatchers(HttpMethod.GET, "/api/menu/items/**").permitAll() // GET requests don't need auth
+      .anyRequest()
+      .authenticated()
+      .and()
+      .oauth2ResourceServer()
+      .jwt();
+  }
+}
