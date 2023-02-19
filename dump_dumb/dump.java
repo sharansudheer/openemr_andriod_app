1b4dd7e01ad14cdc8ee84845ec151fad25a31de9
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-02-19 23:41:27 +0530

    Dumped Errors

--- dump_dumb/dump.java
+++ dump_dumb/dump.java
@@ -280,3 +280,15 @@ username: Its value is muhammed. This field represents the username of the user 
 password: Its value is 1234. This field represents the password of the user who is trying to authenticate.
 
 */
+ 
+/*
+const formData = new FormData();
+formData.append("Scope", "openid offline_access api:oemr api:fhir user/allergy.read user/allergy.write user/appointment.read user/appointment.write user/dental_issue.read user/dental_issue.write user/document.read user/document.write user/drug.read user/encounter.read user/encounter.write user/facility.read user/facility.write user/immunization.read user/insurance.read user/insurance.write user/insurance_company.read user/insurance_company.write user/insurance_type.read user/list.read user/medical_problem.read user/medical_problem.write user/medication.read user/medication.write user/message.write user/patient.read user/patient.write user/practitioner.read user/practitioner.write user/prescription.read user/procedure.read user/soap_note.read user/soap_note.write user/surgery.read user/surgery.write user/transaction.read user/transaction.write user/vital.read user/vital.write user/AllergyIntolerance.read user/CareTeam.read user/Condition.read user/Coverage.read user/Encounter.read user/Immunization.read user/Location.read user/Medication.read user/MedicationRequest.read user/Observation.read user/Organization.read user/Organization.write user/Patient.read user/Patient.write user/Practitioner.read user/Practitioner.write user/PractitionerRole.read user/Procedure.read");
+*/
+
+
+
+/*
+ 
+
+ */
