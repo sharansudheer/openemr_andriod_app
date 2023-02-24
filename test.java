1aad9e327c3ae55bb31cf2653a966357d7f42f91
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-02-24 23:57:38 +0530

    Update test.java

--- test.java
+++ test.java
@@ -175,17 +175,5 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-      .mvcMatchers(HttpMethod.GET, "/api/menu/items/**").permitAll() // GET requests don't need auth
-      .anyRequest()
-      .authenticated()
-      .and()
-      .oauth2ResourceServer()
-      .jwt();
-  }
-}
+@EnableWebSe
 //test
