50cf2ca360ae4c3970ca73c3870cb8be110227ac
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-03-06 23:29:17 +0530

    Login Controller

--- app/src/main/java/com/example/main_application/LoginActivity.java
+++ app/src/main/java/com/example/main_application/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.main_application;
-
+import com.apicontroller.Authorize;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
+
+        Button button = findViewById(R.id.submit_login);
+        button.setOnClickListener(v -> {
+            // code to be executed when button is clicked
+            EditText nameField = findViewById(R.id.get_name);
+            String username = nameField.getText().toString();
+
+            EditText passField = findViewById(R.id.get_password);
+            String password = passField.getText().toString();
+
+
+        });
     }
 }
+
+/*
+Button button = (Button) findViewById(R.id.submit_login);
+
+button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)*/
