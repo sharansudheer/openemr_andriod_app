24c7e84c752e3ab83133ff54d93bd49963468f47
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-03-07 23:22:05 +0530

    Build error fixing

--- app/src/main/java/com/example/main_application/LoginActivity.java
+++ app/src/main/java/com/example/main_application/LoginActivity.java
@@ -1,32 +1,47 @@
 package com.example.main_application;
 import com.apicontroller.Authorize;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 public class LoginActivity extends AppCompatActivity {
+    public void LoginHandler(View v) {
+        // code to be executed when button is clicked
+        EditText nameField = findViewById(R.id.get_name);
+        String username = nameField.getText().toString();
+
+        EditText passField = findViewById(R.id.get_password);
+        String password = passField.getText().toString();
+        Authorize tokengen = new Authorize();
+        try {
+            tokengen.GetAccessToken(username, password);
+        } catch (HttpClientErrorException ex) {
+            if (ex.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                Toast.makeText(this, "Check Your User Name or Password", Toast.LENGTH_SHORT).show();
+            } else if (ex.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                Toast.makeText(this, "Internal Error 400", Toast.LENGTH_SHORT).show();
+            } else if (ex.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR) {
+                Toast.makeText(this, "Internal Error 500", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
 
-        Button button = findViewById(R.id.submit_login);
-        button.setOnClickListener(v -> {
-            // code to be executed when button is clicked
-            EditText nameField = findViewById(R.id.get_name);
-            String username = nameField.getText().toString();
 
-            EditText passField = findViewById(R.id.get_password);
-            String password = passField.getText().toString();
-
-
-        });
     }
 }
+
 
 /*
 Button button = (Button) findViewById(R.id.submit_login);
