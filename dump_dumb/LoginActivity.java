7e035311293ac6ccdf0e0cbe93c778870a17f530
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-03-08 23:35:56 +0530

    Build error fixing

---
+++ dump_dumb/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.main_application;
+import com.apicontroller.Authorize;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
+
+public class LoginActivity extends AppCompatActivity {
+    public void LoginHandler(View v) {
+        // code to be executed when button is clicked
+//        EditText nameField = findViewById(R.id.get_name);
+//        String username = nameField.getText().toString();
+//
+//        EditText passField = findViewById(R.id.get_password);
+//        String password = passField.getText().toString();
+        Authorize tokengen = new Authorize();
+        String userString = "admin";
+        String passString = "i-0006671ac595ee909";
+        try {
+            tokengen.GetAccessToken(userString, passString);
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+        } catch (HttpClientErrorException ex) {
+            if (ex.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                Toast.makeText(this, "Check Your User Name or Password", Toast.LENGTH_SHORT).show();
+            } else if (ex.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                Toast.makeText(this, "Internal Error 400", Toast.LENGTH_SHORT).show();
+            } else if (ex.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR) {
+                Toast.makeText(this, "Internal Error 500", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_main);
+
+
+    }
+}
+
+
+/*
+Button button = (Button) findViewById(R.id.submit_login);
+
+button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)*/
