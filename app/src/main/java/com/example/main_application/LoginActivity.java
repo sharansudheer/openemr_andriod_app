6ce83d7c97d9d2e0e9e287a35211dfa05a6c36b0
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-03-10 23:20:02 +0530

    Login Controller

--- app/src/main/java/com/example/main_application/LoginActivity.java
+++ app/src/main/java/com/example/main_application/LoginActivity.java
@@ -1,23 +1,66 @@
 package com.example.main_application;
-//import com.apicontroller.Authorize;
-
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import retrofit2.Callback;
+
 import android.widget.Toast;
 
 
+import com.apicontroller.AuthResponse;
+import com.apicontroller.Authorize;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
+    private Context context;
     public void LoginHandler(View v) {
+
+        // Initialize the context variable
+        context = getApplicationContext();
+
         // code to be executed when button is clicked
         EditText nameField = findViewById(R.id.get_name);
         String username = nameField.getText().toString();
-
         EditText passField = findViewById(R.id.get_password);
         String password = passField.getText().toString();
+
+        // Use the context variable to get an instance of the AccountManager class
+        AccountManager accountManager = AccountManager.get(context);
+        Account account = new Account(username, "com.example.main_application.ACCOUNT_TYPE");
+        accountManager.addAccountExplicitly(account, password, null);
+
+        Authorize.ApiService apiService = retrofit.create(Authorize.ApiService.class);
+        Call<AuthResponse> call = apiService.authenticateUser(
+                "password",
+                "",
+                "",
+                "users",
+                username,
+                password
+        );
+        call.enqueue(new Callback<AuthResponse>() {
+            @Override
+            public void onResponse(Call<AuthResponse> call, Response<AuthResponse> response) {
+                if (response.isSuccessful()) {
+                    AuthResponse tokenResponse = response.body();
+                    // Do something with the tokenResponse
+                } else {
+                    // Handle error
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AuthResponse> call, Throwable t) {
+                // Handle error
+            }
+        });
     }
 
     @Override
