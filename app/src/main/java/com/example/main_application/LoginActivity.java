da950c0363d36934e828245d999c2bd68fbbf485
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-03-11 23:54:40 +0530

    Login Controller

--- app/src/main/java/com/example/main_application/LoginActivity.java
+++ app/src/main/java/com/example/main_application/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -50,8 +51,9 @@ public class LoginActivity extends AppCompatActivity {
         ApiService apiService = retrofit.create(com.apicontroller.ApiService.class);
         Call<AuthResponse> call = apiService.authenticateUser(
                 "password",
-                "",
-                '',
+                "LnjqojEEjFYe5j2Jp9m9UnmuxOnMg4VodEJj3yE8_OA",
+                "openid offline_access api:oemr api:fhir user/allergy.read user/allergy.write user/appointment.read user/appointment.write user/dental_issue.read user/dental_issue.write user/document.read user/document.write user/drug.read user/encounter.read user/encounter.write user/facility.read user/facility.write user/immunization.read user/insurance.read user/insurance.write user/insurance_company.read user/insurance_company.write user/insurance_type.read user/list.read user/medical_problem.read user/medical_problem.write user/medication.read user/medication.write user/message.write user/patient.read user/patient.write user/practitioner.read user/practitioner.write user/prescription.read user/procedure.read user/soap_note.read user/soap_note.write user/surgery.read user/surgery.write user/transaction.read user/transaction.write user/vital.read user/vital.write user/AllergyIntolerance.read user/CareTeam.read user/Condition.read user/Coverage.read user/Encounter.read user/Immunization.read user/Location.read user/Medication.read user/MedicationRequest.read user/Observation.read user/Organization.read user/Organization.write user/Patient.read user/Patient.write user/Practitioner.read user/Practitioner.write user/PractitionerRole.read user/Procedure.read",
+                "users",
                 username,
                 password
         );
@@ -59,8 +61,10 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<AuthResponse> call, Response<AuthResponse> response) {
                 if (response.isSuccessful()) {
-                    AuthResponse tokenResponse = response.body();
                     // Do something with the tokenResponse
+                    Intent myIntent = new Intent(LoginActivity.this, MainPatientDashboard.class);
+                    //myIntent.putExtra("key", value); //Optional parameters
+                    MainPatientDashboard.startActivity(myIntent);
                 } else {
                     // Handle error
                 }
@@ -72,6 +76,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
