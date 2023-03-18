e0d1b46d01432221e19023e14e8b71c72ca1ddfa
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-03-18 18:09:47 +0530

    Login dump

--- dump_dumb/main_dump.java
+++ dump_dumb/main_dump.java
@@ -48,3 +48,56 @@ public void logout(Account account, String accessToken, Context context) {
     // Clear user data
     // ...
 }
+Account account = new Account("account_name", "account_type");
+AccountManager accountManager = AccountManager.get(context);
+String authToken = "your_access_token";
+boolean accountAdded = accountManager.addAccountExplicitly(account, authToken, null);
+AccountManager accountManager = AccountManager.get(context);
+Account account = new Account("account_name", "account_type");
+String authTokenType = "your_auth_token_type";
+accountManager.getAuthToken(account, authTokenType, null, activity, new AccountManagerCallback<Bundle>() {
+    @Override
+    public void run(AccountManagerFuture<Bundle> future) {
+        try {
+            String authToken = future.getResult().getString(AccountManager.KEY_AUTHTOKEN);
+            // Use the authToken to make API requests
+        } catch (OperationCanceledException | AuthenticatorException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}, null);
+
+
+@Override
+public void onResponse(Call<AuthResponse> call, Response<AuthResponse> response) {
+    if (response.isSuccessful()) {
+        // Store access token in Account Manager
+        AccountManager accountManager = AccountManager.get(getApplicationContext());
+        Account account = new Account(username, "com.example.myapp.account");
+        accountManager.addAccountExplicitly(account, null, null);
+        accountManager.setAuthToken(account, "access_token", response.body().getAccessToken());
+
+        // Launch Dashboard activity
+        Intent intent = new Intent(this, DashboardActivity.class);
+        startActivity(intent);
+    } else {
+        Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show();
+    }
+}
+
+// Check if access token is already present in Account Manager
+AccountManager accountManager = AccountManager.get(getApplicationContext());
+Account[] accounts = accountManager.getAccountsByType("com.example.myapp.account");
+if (accounts.length > 0) {
+    String accessToken = accountManager.peekAuthToken(accounts[0], "access_token");
+    if (accessToken != null) {
+        // Launch Dashboard activity
+        Intent intent = new Intent(this, DashboardActivity.class);
+        startActivity(intent);
+        return; // Don't launch Login activity
+    }
+}
+
+// Launch Login activity
+Intent intent = new Intent(this, LoginActivity.class);
+startActivity(intent);
