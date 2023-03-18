d472e5315e2ebb45d84ecccfa5e0be3c2dafb9fc
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-03-18 17:20:37 +0530

    Login Successful

--- app/src/main/java/com/apicontroller/ApiService.java
+++ app/src/main/java/com/apicontroller/ApiService.java
@@ -1,23 +1,17 @@
 package com.apicontroller;
 
 
+import java.util.Map;
+
 import retrofit2.Call;
-import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
-
 
 public interface ApiService {
         @FormUrlEncoded
         @POST("oauth2/default/token")
-        Call<AuthResponse> authenticateUser(
-                @Field("grant_type") String grantType,
-                @Field("client_id") String clientId,
-                @Field("scope") String scope,
-                @Field("user_role") String userRole,
-                @Field("username") String username,
-                @Field("password") String password
-        );
+        Call<AuthResponse> authenticateUser(@FieldMap Map<String, Object> map);
 
 
 
