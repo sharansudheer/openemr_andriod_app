348fe370c75109db7e4d528bd1b7098b7628f242
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-03-23 23:14:26 +0530

    In progress

--- app/src/main/java/com/example/main_application/MainPatientDashboard.java
+++ app/src/main/java/com/example/main_application/MainPatientDashboard.java
@@ -4,6 +4,7 @@ package com.example.main_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,30 +15,44 @@ public class MainPatientDashboard extends AppCompatActivity {
     Button call_prescription;
     Button call_allergies;
     Button call_summary;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_patient_dashboard);
 
+        // Get shared preferences object
+        sharedPreferences = getSharedPreferences("MY_APP_PREFS", MODE_PRIVATE);
+        // Check if user is logged in
+        boolean isLoggedIn = sharedPreferences.getBoolean("IS_LOGGED_IN", false);
+        if (isLoggedIn) {
+            // User is already logged in, start dashboard activity
+            Intent intent = new Intent(this, MainPatientDashboard.class);
+            startActivity(intent);
+            call_appointment = (Button) findViewById(R.id.goto_appointments);
+            call_appointment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openNewActivity();
+                }
+            });
 
-        call_appointment = (Button) findViewById(R.id.goto_appointments);
-        call_appointment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNewActivity();
-            }
-        });
+            call_billing = (Button) findViewById(R.id.goto_billing);
+            call_billing.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openNewBilling();
+                }
+            });
 
-
-
-        call_billing = (Button) findViewById(R.id.goto_billing);
-        call_billing.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNewBilling();
-            }
-        });
+            finish();
+        } else {
+            // User is not logged in, start login activity
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     public void openNewActivity(){
