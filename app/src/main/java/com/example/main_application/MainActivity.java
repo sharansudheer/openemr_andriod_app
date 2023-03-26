b1cd17b189c6b5614b2510ee756ed1415bd9ca69
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2023-03-26 22:41:26 +0530

    Retrofitted Login Using RoomDB

---
+++ app/src/main/java/com/example/main_application/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.main_application;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Check if the user is logged in or not
+        SharedPreferences sharedPreferences = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+
+        // Start the appropriate activity based on the login status
+        Intent intent;
+        if (isLoggedIn) {
+            intent = new Intent(this, MainPatientDashboard.class);
+        } else {
+            intent = new Intent(this, LoginActivity.class);
+        }
+        startActivity(intent);
+        finish();
+    }
+}
+
--- app/src/main/java/com/example/main_application/MainPatientDashboard.java
+++ app/src/main/java/com/example/main_application/MainPatientDashboard.java
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import android.view.View;
 import android.widget.Button;
 
@@ -22,39 +27,49 @@ public class MainPatientDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_patient_dashboard);
 
-         //Get shared preferences object
-        sharedPreferences = getSharedPreferences("MY_APP_PREFS", MODE_PRIVATE);
-        // Check if user is logged in
-        boolean isLoggedIn = sharedPreferences.getBoolean("IS_LOGGED_IN", false);
-        Intent intent;
-        if (!isLoggedIn) {
-            // User is not logged in, start login activity
-            intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+        call_appointment = findViewById(R.id.goto_appointments);
+        call_appointment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
 
-
-        } else {
-
-
-            call_appointment = findViewById(R.id.goto_appointments);
-            call_appointment.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openNewActivity();
-                }
-            });
-
-            call_billing = findViewById(R.id.goto_billing);
-            call_billing.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openNewBilling();
-                }
-            });
-
-        }
-        finish();
+        call_billing = findViewById(R.id.goto_billing);
+        call_billing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewBilling();
+            }
+        });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dashboard_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logOut();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logOut() {
+        // log out logic here
+        SharedPreferences sharedPreferences = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("isLoggedIn", false);
+        editor.apply();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+//        setResult(RESULT_OK);
+//        finish();
+    }
+
 
     public void openNewActivity(){
         Intent intent = new Intent(this, Appointments.class);
@@ -65,10 +80,6 @@ public class MainPatientDashboard extends AppCompatActivity {
         startActivity(intent);
 
     }
-    private void onUserLoggedOut() {
-        // Set the result and finish the activity
-        setResult(RESULT_OK);
-        finish();
-    }
+
 
 }
