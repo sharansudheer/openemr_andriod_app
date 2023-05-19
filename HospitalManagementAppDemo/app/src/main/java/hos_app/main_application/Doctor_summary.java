ced796ae19f9fb39a252fdc2ab34ec74114f130e
sharansudheer <saran_sudheer@hotmail.com>
2023-05-19 23:50:40 +0530

    Doctor Portal In Progress

--- HospitalManagementAppDemo/app/src/main/java/hos_app/main_application/Doctor_summary.java
+++ HospitalManagementAppDemo/app/src/main/java/hos_app/main_application/Doctor_summary.java
@@ -2,13 +2,59 @@ package hos_app.main_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+import java.util.Objects;
 
 public class Doctor_summary extends AppCompatActivity {
 
+    MaterialToolbar toolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_doctor_summary);
+        setContentView(R.layout.activity_doctor_prescription);
+        toolBar=findViewById(R.id.topAppBar);
+        setSupportActionBar(toolBar);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home)  {
+            Intent intent = new Intent(Doctor_prescription.this,DoctorDashboard.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        else if (id == R.id.action_logout) {
+            logOut();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dashboard_menu, menu);
+        return true;
+    }
+    private void logOut() {
+        // log out logic here
+        SharedPreferences sharedPreferences = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("isLoggedIn", false);
+        editor.apply();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        setResult(RESULT_OK);
+        finish();
     }
 }
